82b3fff8de79925cdc531546b603062e06b7975d
Chuanqi Sun <1895289+chuanqisun@users.noreply.github.com>
2020-10-10 16:10:55 -0700

    test environment varible

--- functions/get-environments.js
+++ functions/get-environments.js
@@ -24,6 +24,6 @@ exports.handler = async function(event, context, callback) {
   // your server-side functionality
   callback(null, {
     statusCode: 200,
-    body: "Hello, World"
+    body: `token: ${process.env.GITHUB_TOKEN}`
   });
 }
