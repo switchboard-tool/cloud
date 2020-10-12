9a4cec8854f6b4dec48b014e51d370b46820d528
Chuanqi Sun <1895289+chuanqisun@users.noreply.github.com>
2020-10-11 18:57:12 -0700

    remove hard coded link

--- src/components/app-content/app-content.component.ts
+++ src/components/app-content/app-content.component.ts
@@ -41,10 +41,7 @@ export class AppContentComponent extends HTMLElement {
                 <article class="auth-help">
                   <p>
                     Did you encounter issue signing in? <a href=${`${location.protocol}//${location.host}`}>Click here to start over</a> or report the issue in
-                    <a
-                      href="https://teams.microsoft.com/l/channel/19%3a847ddf12dcee4e27adb9c0ff6ab68aa5%40thread.skype/General?groupId=da3b2d71-1ea2-48e2-af0e-cc54e80c1a85&tenantId=72f988bf-86f1-41af-91ab-2d7cd011db47"
-                      >Switchboard Teams General channel</a
-                    >.
+                    <a href=${links.switchboardTeamsGeneralChannel}>Switchboard Teams General channel</a>.
                   </p>
                 </article>
               </div>`
