7ba5091bd832b337beb56672d44d31e6f104b578
Chuanqi Sun <1895289+chuanqisun@users.noreply.github.com>
2020-10-11 22:07:47 -0700

    use ms graph for token validation

--- functions/get-environments.js
+++ functions/get-environments.js
@@ -1,41 +1,33 @@
 const { default: axios } = require("axios");
-const jwt = require("jsonwebtoken");
 const { Octokit } = require("@octokit/rest");
 const yaml = require('js-yaml');
 
-const ALLOWED_AUD = ["00000003-0000-0000-c000-000000000000", "https://graph.microsoft.com"];
-const REQUIRED_SCOPE = "User.Read";
-const ISS = "https://sts.windows.net/72f988bf-86f1-41af-91ab-2d7cd011db47/";
-
 exports.handler = async function(event, context, callback) {
-
-  const publicKeys = (await axios.get("https://login.microsoftonline.com/common/discovery/keys")).data.keys;
-
   
-  const token = event.headers["authorization"].replace("Bearer ", "");
-  const {header, payload} = jwt.decode(token, {complete: true});
-  
-  const {kid} = header;
+  let isValid = true;
 
-  if (!ALLOWED_AUD.includes(payload.aud)) throw new Error("invalid token");
-  if (!payload.scp.includes(REQUIRED_SCOPE)) throw new Error("invalid token");
-  if (ISS !== payload.iss) throw new Error("invalid token");
-  if (!publicKeys.some(key => key.kid === kid)) throw new Error("invalid token");
+  const graphUserResponse = await axios.get("https://graph.microsoft.com/v1.0/me", {headers: {
+    "Authorization": event.headers["authorization"],
+  }});
+
+  if (graphUserResponse.status !== 200) isValid = false;
+  if (!graphUserResponse.data.userPrincipalName.includes("@microsoft.com")) isValid = false;
+
+  if (!isValid) throw new Error("invalid token");
+
+  console.log('Access token is valid');
 
   const octokit = new Octokit({
     auth: process.env.GITHUB_TOKEN,
   });
 
-  console.log('will get environments from github')
-
   const environmentsFileResponse = await octokit.repos.getContent({repo: "environments", owner: "switchboard-tool", path: "environments.yaml"})
   const environments = environmentsFileResponse.data.content;
 
   const content = Buffer.from(environments, 'base64').toString()
-
-  console.log(content);
   const contentObject = yaml.safeLoad(content);
 
+  console.log(`received ${contentObject.length} environments`);
 
   callback(null, {
     statusCode: 200,
