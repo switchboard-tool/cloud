fde6aaed364a7447421d562c5ed0d1a9548f65a6
Chuanqi Sun <chuanqi.sun.dev@gmail.com>
2022-03-10 13:59:34 -0800

    fix breaking change

--- functions/get-environments.js
+++ functions/get-environments.js
@@ -1,42 +1,43 @@
 const { default: axios } = require("axios");
 const { Octokit } = require("@octokit/rest");
-const yaml = require('js-yaml');
+const yaml = require("js-yaml");
 
-exports.handler = async function(event, context, callback) {
-  
+exports.handler = async function (event, context, callback) {
   let isValid = true;
 
-  const graphUserResponse = await axios.get("https://graph.microsoft.com/v1.0/me", {headers: {
-    "Authorization": event.headers["authorization"],
-  }});
+  const graphUserResponse = await axios.get("https://graph.microsoft.com/v1.0/me", {
+    headers: {
+      Authorization: event.headers["authorization"],
+    },
+  });
 
   if (graphUserResponse.status !== 200) isValid = false;
   if (!graphUserResponse.data.userPrincipalName.includes("@microsoft.com")) isValid = false;
 
   if (!isValid) throw new Error("invalid token");
 
-  console.log('Access token is valid');
+  console.log("Access token is valid");
 
   const octokit = new Octokit({
     auth: process.env.GITHUB_TOKEN,
   });
 
-  const environmentsFileResponse = await octokit.repos.getContent({repo: "environments", owner: "switchboard-tool", path: "environments.yaml"})
+  const environmentsFileResponse = await octokit.repos.getContent({ repo: "environments", owner: "switchboard-tool", path: "environments.yaml" });
   const environments = environmentsFileResponse.data.content;
 
-  const timestamp = (new Date(environmentsFileResponse.headers["last-modified"]).toISOString());
+  const timestamp = new Date(environmentsFileResponse.headers["last-modified"]).toISOString();
 
-  const content = Buffer.from(environments, 'base64').toString();
-  const contentObject = yaml.safeLoad(content);
+  const content = Buffer.from(environments, "base64").toString();
+  const contentObject = yaml.load(content);
 
   console.log(`received ${contentObject.length} environments, last modified: ${timestamp}`);
 
   callback(null, {
     statusCode: 200,
     headers: {
-      "Content-Type" : "application/json",
+      "Content-Type": "application/json",
       "Last-Modified": timestamp,
     },
-    body: JSON.stringify(contentObject)
+    body: JSON.stringify(contentObject),
   });
-}
+};
